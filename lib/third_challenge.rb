2aa17fb8fcc0306137257fb97c816cc7666095c1
Daniel Gould <dmgould310@gmail.com>
2020-08-01 13:02:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,24 +2,24 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-       :name => "Lord Montague",
-       :age => "53"
+       name: "Lord Montague",
+       age: => "53"
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        name: "Lady Montague",
+        age: "54"
         },
       :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "Alive"
+        name: "Romeo",
+        age: "15",
+        status: "Alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "51"
+      patriarch: {
+        name: "Lord Capulet",
+        age: "51"
         },
       :matriarch => {
         :name => "Lady Capulet",
